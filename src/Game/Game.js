2e354db638aaf3dab0c96aca7ef34defe93da148
FabrizioDeFiore <fabriziodefiore@gmail.com>
2022-02-24 18:59:38 +0000

    add ball sprite

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,3 +1,5 @@
+import { GameObject } from "../engine/EngineCore";
+
 const Game = {
   Assets: [
     {
@@ -8,8 +10,13 @@ const Game = {
 
   Setup: (engine) => {
     let room = engine.CreateRoom("room0", 800, 600);
+    let circle = new GameObject("circle")
+    engine.Instantiate(circle)
+    circle.sprites.push(engine.images[0])
+    circle.scale = 0.1 
     console.log(engine);
   },
 };
+
 
 export default Game;
